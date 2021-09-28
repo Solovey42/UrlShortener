64fd17f7c10bf191c7fe25618f5282c41f988410
Slava <solovey.1999@mail.ru>
2021-09-28 19:58:34 +0700

    Created repository base class and interface for links

---
+++ API/Domain/Repositories/ILinkRepository.cs
@@ -0,0 +1,11 @@
+﻿using API.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Domain.Repositories
+{
+    public interface ILinkRepository
+    {
+        Task<IEnumerable<Link>> ListAsync();
+    }
+}
---
+++ API/Domain/Repositories/LinkRepository.cs
@@ -0,0 +1,19 @@
+﻿using API.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Domain.Repositories
+{
+    public class LinkRepository : BaseRepository, ILinkRepository
+    {
+        public LinkRepository(LinksContext context) : base(context)
+        {
+
+        }
+        public async Task<IEnumerable<Link>> ListAsync()
+        {
+            return await _context.Links.ToListAsync(); 
+        }
+    }
+}
