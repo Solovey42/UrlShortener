5009ccecf6dd0dcff2c869b3e919b0b689c09d6f
Slava <solovey.1999@mail.ru>
2021-09-28 20:00:25 +0700

    Created service and interface for link minification logic

---
+++ API/Domain/Services/ILinkService.cs
@@ -0,0 +1,11 @@
+﻿using API.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Domain.Services
+{
+    public interface ILinkService
+    {
+        Task<IEnumerable<Link>> ListAsync();
+    }
+}
---
+++ API/Domain/Services/LinkService.cs
@@ -0,0 +1,21 @@
+﻿using API.Domain.Models;
+using API.Domain.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Domain.Services
+{
+    public class LinkService : ILinkService
+    {
+        ILinkRepository _linkRepository;
+        
+        public LinkService(ILinkRepository linkRepository)
+        {
+            _linkRepository = linkRepository;
+        }
+        public Task<IEnumerable<Link>> ListAsync()
+        {
+            return _linkRepository.ListAsync();
+        }
+    }
+}
