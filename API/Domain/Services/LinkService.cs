98cfc3292fa0d17835ee0adf1a1c4820aca37082
Slava <solovey.1999@mail.ru>
2021-10-01 15:51:37 +0700

    Updated regexp

--- API/Domain/Services/LinkService.cs
+++ API/Domain/Services/LinkService.cs
@@ -28,7 +28,7 @@ namespace API.Domain.Services
         public async Task<Link> AddLinkAsync(string longAddress)
         {
 
-            Regex regex = new Regex(@"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)");
+            Regex regex = new Regex(@"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
             if (!regex.IsMatch(longAddress)) 
             {
                 return null;
